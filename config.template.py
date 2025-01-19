6edaec68c1e83d020197c9e43d7a035ec912a174
markliu22 <markliu202@gmail.com>
2025-01-19 13:27:34 -0500

    init commit

---
+++ config.template.py
@@ -0,0 +1,16 @@
+# Twitter API credentials
+TWITTER_API_KEY = "your_twitter_api_key"
+TWITTER_API_SECRET = "your_twitter_api_secret"
+TWITTER_ACCESS_TOKEN = "your_twitter_access_token"
+TWITTER_ACCESS_TOKEN_SECRET = "your_twitter_access_token_secret"
+
+# Twilio SMS credentials
+TWILIO_ACCOUNT_SID = "your_twilio_account_sid"
+TWILIO_AUTH_TOKEN = "your_twilio_auth_token"
+TWILIO_PHONE_NUMBER = "your_twilio_phone_number"
+
+# Email credentials
+EMAIL_ADDRESS = "your_email@example.com"
+EMAIL_PASSWORD = "your_email_password"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587 
